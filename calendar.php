db85616e6833903bbec1c731630ec0e23a2b5a0e
Jido Park <parkmap@gmail.com>
2017-11-08 19:28:05 +0900

    Update calendar.php

--- calendar.php
+++ calendar.php
@@ -1,3 +1,15 @@
+F j, Y g:i a - November 6, 2010 12:50 am
+F j, Y - November 6, 2010
+F, Y - November, 2010
+g:i a - 12:50 am
+g:i:s a - 12:50:48 am
+l, F jS, Y - Saturday, November 6th, 2010
+M j, Y @ G:i - Nov 6, 2010 @ 0:50
+Y/m/d \a\t g:i A - 2010/11/06 at 12:50 AM
+Y/m/d \a\t g:ia - 2010/11/06 at 12:50am
+Y/m/d g:i:s A - 2010/11/06 12:50:48 AM
+Y/m/d - 2010/11/06
+
 $get3lettermonth  = array(
 	'JAN',
 	'FEB',
